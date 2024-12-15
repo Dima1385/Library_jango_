fa445f334617b15f8bbeb9a10b4009d529254f0a
Dima1385 <Lozik1385@gmail.com>
2024-12-15 11:43:07 +0200

    fix

--- library/models.py
+++ library/models.py
@@ -23,6 +23,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    pages = models.PositiveIntegerField() 
+    genre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
